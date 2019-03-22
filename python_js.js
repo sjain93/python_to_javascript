d79a673545157c64fa19d329220da9c2051778db
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-03-22 17:40:33 -0400

    Initial commit

---
+++ python_js.js
@@ -0,0 +1,40 @@
+// let apples = 14;
+// console.log(apples);
+//
+// console.log(`I have ${apples} apples`);
+//
+// const materials = ['wood', 'metal', 'stone'];
+// const words = {};
+// words.elephant = "The world's largest land mammal";
+// words.school = 'A place of learning.';
+// words.ice_cream = 'A delicious milk-based dessert';
+//
+// console.log(words['ice_cream']);
+//
+// // let num = 16;
+// //
+// // if (num > 10) {
+// //   console.log(`${num} is greater than 10`);
+// // }
+// // else if (num == 10){
+// //   console.log(`${num} is exactly 10`);
+// // }
+// // else {
+// //   console.log(`${num} is less than 10`);
+// // }
+// //
+// // for (x=0; x<10; x++){
+// // //   console.log("Doing the same thing over and over");
+// // }
+//
+// let base = 5;
+// for (num=0; num<20; num++){
+//   console.log(num+base);
+// }
+
+let total = 0;
+for (num=0; num<100; num++){
+  total += num;
+}
+
+console.log(total);
